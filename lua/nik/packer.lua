a84759523834d36fd542ec693e9e70de62ad4f02
Nikhil Suresh <nikhilsuresh@hotmail.com>
2023-10-24 14:50:00 +1100

    Add markdown-previewer to aid brainstorming

--- lua/nik/packer.lua
+++ lua/nik/packer.lua
@@ -47,6 +47,8 @@ use {
 },
 
 use ( 'github/copilot.vim' ),
-use ( 'rstacruz/vim-closer')
+use ( 'rstacruz/vim-closer'),
+use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+
 }
 end)
